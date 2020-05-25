451548d8a8f5218c57683276bf61c2cf0b1dafde
MEICHENLIN <47967325+MEICHENLIN@users.noreply.github.com>
2020-05-25 17:32:29 +1000

    Update app.js

--- express-server/app.js
+++ express-server/app.js
@@ -1,3 +1,4 @@
+/*
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'reports')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -46,4 +48,103 @@ app.use(function(err, req, res, next) {
 
 module.exports = app;
 
+ */
 
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+//var indexRouter = require('./routes/index');
+//app.use('/', indexRouter);
+
+router.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/chart.html'));
+
+});
+
+//add the router
+app.use('/', router);
+app.listen(process.env.port || 3000);
+
+console.log('Running at Port 3000');
+
+
+const nano = require('nano')('http://admin:instance1@172.26.132.103:5984');
+
+
+
+// get a database
+const twitter_search = nano.use('twitter-search');
+
+
+//get a couchdb view (a bunch of tweets)
+/*
+twitter_search.view('location', 'locationSent').then((body) => {
+    body.rows.forEach((doc) => {
+        console.log(doc);
+    });
+});
+ */
+
+
+//const forbes = "https://forbes400.herokuapp.com/api/forbes400?limit=5"
+const melSum = "http://admin:instance1@172.26.132.103:5984/twitter-search/_design/location/_view/locationSent?limit=5";
+const melCount = "http://admin:instance1@172.26.132.103:5984/twitter-search/_design/location/_view/melCount?limit=5";
+const rp = require('request-promise');
+
+function processData(rp, melCount, melSum){
+    rp(melCount).then((melcount) => {
+        var json_count = JSON.parse(melcount);
+        console.log(json_count);
+        var count = json_count.rows[0].value;
+
+        console.log(count);
+        rp(melSum).then((melsum) => {
+            /*
+            console.log(melsum);
+            var json_sum = JSON.parse(melsum);
+            console.log(json_sum);
+            var sum = json_sum.rows[0].value;
+            console.log(json_sum.rows[0].key);
+            console.log(sum);
+            var melAvg = sum / count;
+            console.log(melAvg);
+            var avglist = [3, 0];
+            //console.log(avglist);
+            //return avglist;
+            console.log(json_sum.rows);
+             */
+            var json_sum = JSON.parse(melsum);
+            var labels = json_sum.rows.map(function (e){
+                return e.key;
+            });
+            var values = json_sum.rows.map(function (e){
+                return e.value;
+            });
+            console.log(labels);
+            console.log(values);
+        })
+    })
+}
+module.exports = processData(rp, melCount, melSum);
+
+/*
+rp(forbes).then((response) => {
+    var json = JSON.parse(response);
+    var labels = json.map(function (e){
+        return e.name;
+    });
+    var values = json.map(function (e){
+        return e.finalWorth;
+    });
+    console.log(labels);
+    console.log(values);
+    //var count = json_count.rows[0].value;
+
+    //console.log(count);
+
+    }
+)
+ */
