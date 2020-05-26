6c76255124746d29eafe55e706d4542b4fd14e4d
MEICHENLIN <47967325+MEICHENLIN@users.noreply.github.com>
2020-05-27 06:22:40 +1000

    Update app.js

--- express-server/app.js
+++ express-server/app.js
@@ -8,9 +8,6 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-
-//var nano = require('nano')('http://localhost:5984');
-
 var app = express();
 
 // view engine setup
@@ -46,63 +43,3 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-/*
-const express = require('express');
-const app = express();
-const path = require('path');
-const router = express.Router();
-
-//var indexRouter = require('./routes/index');
-//app.use('/', indexRouter);
-
-router.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/chart.html'));
-
-});
-
-
-//add the router
-app.use('/', router);
-app.listen(process.env.port || 3000);
-
-console.log('Running at Port 3000');
-*/
-
-//const nano = require('nano')('http://admin:instance1@172.26.132.103:5984');
-
-// get a database
-//const twitter_search = nano.use('twitter-search');
-
-
-//get a couchdb view (a bunch of tweets)
-/*
-twitter_search.view('location', 'locationSent').then((body) => {
-    body.rows.forEach((doc) => {
-        console.log(doc);
-    });
-});
- */
-//const forbes = "https://forbes400.herokuapp.com/api/forbes400?limit=5"
-
-/*
-function processData(rp, melCount, melSum){
-    rp(melCount).then((melcount) => {
-        var json_count = JSON.parse(melcount);
-        console.log(json_count);
-        var count = json_count.rows[0].value;
-
-        console.log(count);
-        rp(melSum).then((melsum) => {
-            var json_sum = JSON.parse(melsum);
-            var labels = json_sum.rows.map(function (e){
-                return e.key;
-            });
-            var values = json_sum.rows.map(function (e){
-                return e.value;
-            });
-            console.log(labels);
-            console.log(values);
-        })
-    })
-}
- */
