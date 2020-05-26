111627e1ecc630af7768a3c465e468d5c4b65ecc
MEICHENLIN <47967325+MEICHENLIN@users.noreply.github.com>
2020-05-27 06:17:54 +1000

    Update index.js

--- express-server/routes/index.js
+++ express-server/routes/index.js
@@ -10,9 +10,6 @@ let date;
 let sentiment;
 let tweets;
 
-//const axios = require('axios');
-//const requestUrl = "http://172.26.132.103:5984/twitter-search/_design/location/_view/locationSent"
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
@@ -40,46 +37,22 @@ router.get('/', function(req, res, next) {
                 return sum/count;
             });
 
-            // proportion of tweets per location
+            // total tweets per location
             rp(locationSentiment).then((response) => {
                 var json = JSON.parse(response);
-                /*
+
                 var counts = 0;
                 for(var i=0; i<json.rows.length; i++){
                     counts+=json.rows[i].value.count;
                 }
-                prop = json.rows.map(function (e){
-                    return Math.round((e.value.count/counts)*100);
-                });
-                 */
                 tweets = json.rows.map(function (e){
                     return e.value.count;
                 });
                 res.render('index', { title: 'Dashboard', location: location, avgSentiment: avgSentiment, date: date,
-                    sentiment: sentiment, tweets: tweets});
+                    sentiment: sentiment, tweets: tweets, counts: counts});
             });
             });
         });
     });
 
 module.exports = router;
-
-
-/*
-//get a couchdb document (a tweet)
-twitter_search.get('5703b847514f584955956d187300384a').then((body)=>{
-    console.log(body);
-});
-
-const nano = require('nano')('http://admin:instance1@172.26.132.103:5984');
-
-// get a database
-const twitter_search = nano.use('twitter-search');
-
-//get a couchdb view (a bunch of tweets)
-twitter_search.view('location', 'mel-pos').then((body) => {
-    body.rows.forEach((doc) => {
-        console.log(doc.value);
-    });
-});
-*/
